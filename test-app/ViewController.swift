a3b448c7c53dbd7efdd60e6ef682df2e1f3ade63
Shri <76138254+Shrinivas2708@users.noreply.github.com>
2024-10-14 12:16:00 +0530

    Add files via upload

---
+++ test-app/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  test-app
+//
+//  Created by Apple CSE 40 on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet var outputLabel: UILabel!
+    
+    
+    @IBAction func equals(_ sender: UIButton) {
+        
+    }
+    @IBAction func clearBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func add(_ sender: UIButton) {
+        
+    }
+    @IBAction func dot(_ sender: UIButton) {
+        
+    }
+    @IBAction func mul(_ sender: UIButton) {
+        
+    }
+    @IBAction func subtract(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func zero(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func one(_ sender: UIButton) {
+    }
+    
+    @IBAction func two(_ sender: UIButton) {
+    }
+    
+    @IBAction func three(_ sender: UIButton) {
+    }
+    
+    @IBAction func four(_ sender: UIButton) {
+    }
+    
+    @IBAction func five(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func six(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func seven(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func eight(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func nine(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
